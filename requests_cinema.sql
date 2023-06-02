e9cc87a75c7ff226c37334bd29bb768cb941f977
Skyflexx <118121459+Skyflexx@users.noreply.github.com>
2023-06-02 16:55:17 +0200

    #

--- requests_cinema.sql
+++ requests_cinema.sql
@@ -10,9 +10,9 @@ SELECT
 	
 FROM film f
 	INNER JOIN realisateur r
-	ON f.id_realisateur = r.id_realisateur
+		ON f.id_realisateur = r.id_realisateur
 	INNER JOIN personne p
-	ON r.id_personne = p.id_personne
+		ON r.id_personne = p.id_personne
 
     WHERE id_film = 1 -- Selection des infos qu'on souhaite par l'ID film.
 
@@ -22,7 +22,7 @@ SELECT f.titre_film, df.duree
 
 FROM film f
     INNER JOIN detail_film df
-    ON f.id_film = df.id_film
+   		ON f.id_film = df.id_film
 
 WHERE f.duree_film > 135
 ORDER BY f.duree_film DESC
@@ -35,9 +35,9 @@ SELECT
 	
 FROM film f
 	INNER JOIN realisateur r
-	ON f.id_realisateur = r.id_realisateur
+		ON f.id_realisateur = r.id_realisateur
 	INNER JOIN personne p
-	ON r.id_personne = p.id_personne
+		ON r.id_personne = p.id_personne
 	
 WHERE p.nom = 'Nolan'
 
@@ -47,12 +47,11 @@ SELECT
 	g.nom_genre,
 	COUNT(f.id_film) AS nombre_films
 
-FROM
-	film f
-INNER JOIN appartenir a
-	ON f.id_film = a.id_film
-INNER JOIN genre g
-	ON a.id_genre = g.id_genre
-	
+FROM film f
+	INNER JOIN appartenir a
+		ON f.id_film = a.id_film
+	INNER JOIN genre g
+		ON a.id_genre = g.id_genre	
+
 GROUP BY g.nom_genre
 ORDER BY nombre_films DESC
